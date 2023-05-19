9d878b119ce95fe6ea22ef7760198b9e782a775e
DenDev82 <dendev82@gmail.com>
2023-05-19 17:18:32 +0200

    Isus

--- src/App.js
+++ src/App.js
@@ -1,13 +1,22 @@
 import './App.css';
 import Employee from './components/Employee';
+import { useState } from 'react';
+
 function App() {
+  let role = "dev";
   const showEmployees = true;
   return (
     <div className="App">
+      
       {showEmployees ? 
         <>
-          <Employee/>
-          <Employee/>
+        
+        <input type="text" onChange={(e)=>{
+          console.log(e.target.value);
+          role = e.target.value;
+        }}/>
+          <Employee name="Isus" role = "Intern"/>
+          <Employee name="Denis" role = {role} />
           <Employee/>
         </>
         :
